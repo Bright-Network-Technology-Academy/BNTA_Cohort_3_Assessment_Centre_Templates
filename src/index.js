174f4d25ba6c1c611eb42cc424b47e1ec337e229
darren <darrenm95@icloud.com>
2021-09-07 14:55:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// List all your products here that you have included inside your HTML
-let product1 = {
-  img: "image source here",
-  product: "product1",
-  price: 3.99,
+// Declare a variable to contain a product on your e-commerce page.
+let product = {
+  product: "product",
 };
 
-// let product2 ...
-
-// let product3 ...
-
 // Declare an add product button that relates to the relevant HTML element inside index.html.
-let addProductButton = document.getElementById("addProductToBasket");
+let addProduct1Button = document.getElementById("addProduct1ToBasket");
+let addProduct2Button = document.getElementById("addProduct2ToBasket");
+let addProduct3Button = document.getElementById("addProduct3ToBasket");
 
 // Declare a remove product button that relates to the relevant HTML element inside index.html.
-let removeProductButton = document.getElementById("removeProductFromBasket");
+let removeProduct1Button = document.getElementById("removeProduct1FromBasket");
+let removeProduct2Button = document.getElementById("removeProduct2FromBasket");
+let removeProduct3Button = document.getElementById("removeProduct3FromBasket");
 
 // Declare an empty array to store all items added to your basket.
 let basket = [];
@@ -28,6 +26,9 @@ function addProductToBasket(product) {
   // CODING_TASK_9: Below this line write the code that will take the product
   // parameter and add it to your pages basket. Make sure you reflect the 
   // number of items in your basket on your page.
+  
+  //ENTER CODE HERE
+
   updateHTML();
 }
 
@@ -36,15 +37,29 @@ function removeProductFromBasket(index) {
   // CODING_TASK_10: Below this line write the code that will take the index
   // parameter and use it to remove the selected item from your pages basket.
   // Make sure you reflect the number of items in your basket on your page.
+  
+  //ENTER CODE HERE
+
   updateHTML();
 }
 
-// Add event listener to all buttons with the addProductToBasket id.
-addProductButton.addEventListener("click", () => {
-  addProductToBasket(product1);
+// Add event listener to all buttons with the addProductToBasket id<.
+addProduct1Button.addEventListener("click", () => {
+  addProductToBasket(product);
 });
-
-// Add event listener to all buttons with the removeProductFromBasket id.
-removeProductButton.addEventListener("click", () => {
-  removeProductFromBasket(basket[-1]);
+addProduct2Button.addEventListener("click", () => {
+  addProductToBasket(product);
+});
+addProduct3Button.addEventListener("click", () => {
+  addProductToBasket(product);
+});
+// Add event listener to all buttons with the removeProductFromBasket id to remove last item in basket.
+removeProduct1Button.addEventListener("click", () => {
+  removeProductFromBasket((basket.length)-1);
+});
+removeProduct2Button.addEventListener("click", () => {
+  removeProductFromBasket((basket.length)-1);
+});
+removeProduct3Button.addEventListener("click", () => {
+  removeProductFromBasket((basket.length)-1);
 });
